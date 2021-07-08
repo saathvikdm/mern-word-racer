aedc21ce1aff6363271a4370bf560f23b14b4fa5
saathvikdm <saathvikdm@outlook.com>
2021-07-09 02:08:27 +0530

    game early demo playble in dev console

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import { useState, useEffect } from "react";
 
 const keyPressArray = [];
 let index = 0;
+let letterIndex = 0;
+
 const word = 'Example';
+const wordList = ['example', 'second', 'demo']
 
 function App() {
 
   
   const [keyPressStack, setKeyPressedStack] = useState([]);
   const [correct , setCorrect] = useState(false);
-  
 
   // Call our hook for each key that we'd like to monitor
-  const happyPress = useKeyPress(word[index], setKeyPressedStack, setCorrect);
-  
 
-  
+  const happyPress = useKeyPress(wordList[index][letterIndex], setKeyPressedStack, setCorrect);  
 
   return (
     <div>
-      <div>{word}</div>
+      <div>{wordList.toString()}</div>
       <div>
         {happyPress && "😊"}<br/>
         {keyPressStack.toString()}
@@ -31,20 +31,41 @@ function App() {
 }
 // Hook
 function useKeyPress(targetKey, setKeyPressedStack, setCorrect) {
-  
+  const wordListLength = wordList.length;
+  console.log(wordList[index].length);
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = useState(false);
 
   // If pressed key is our target key then set to true
   function downHandler({ key }) {
-    if (key === targetKey) {
-      keyPressArray.push(key);
-      index++;
-      setKeyPressedStack(keyPressArray)
-      setKeyPressed(true);
-      console.log(keyPressArray.toString());
-      if(keyPressArray.join('') === word) { setCorrect(true);  alert('COrrects');}
+    
+    keyPressArray.push(key);
+    console.log(keyPressArray.join(''));
+    if(keyPressArray.join('') === wordList[index]) {
+      console.log("Point");
+      keyPressArray.length = 0;
+      index === wordListLength - 1 ? index = 0 : index++;
+      console.log('index' + index);
+      // console.log(wordList[index].length);
     }
+
+    if(keyPressArray.length === wordList[index].length) {
+      keyPressArray.length = 0;
+      index === wordListLength - 1 ? index = 0 : index++;
+      console.log('index' + index);
+      console.log(wordList[index].length);
+      console.log('Wrong word! No point.');
+    }
+
+
+    // if (key === targetKey) {
+    //   keyPressArray.push(key);
+    //   letterIndex++;
+    //   setKeyPressedStack(keyPressArray)
+    //   setKeyPressed(true);
+    //   console.log(keyPressArray.toString());
+    //   // if(keyPressArray.join('') === word) { while(keyPressArray.length) { setKeyPressed(false); keyPressArray.pop()}; setKeyPressedStack([]); index++; console.log(keyPressArray); letterIndex = 0;}
+    // }
   }
   // If released key is our target key then set to false
   const upHandler = ({ key }) => {
