4efa535950fc16a6df0c60db4713b2995b06f7af
saathvikdm <saathvikdm@outlook.com>
2021-07-14 12:26:25 +0530

    Added is empty validation for saving score

--- src/pages/SaveScorePage.jsx
+++ src/pages/SaveScorePage.jsx
@@ -12,26 +12,36 @@ export default function SaveScorePage() {
     const { score, level } = useContext(ScoreContext);
     const [username, setUsername] = useState(null);
     const [saved, setSaved] = useState(0);
+    const [success, setSuccess] = useState(0);
 
     const data = { username, score, level };
 
     function handleSave() {
+        if(!username) {
+            setSuccess(2);
+            setTimeout(() => { setSuccess(0) }, 1500);
+        } else {
         setSaved(1);
         axios
             .post(`https://word-racer-server.herokuapp.com/scores`, data)
             .then((res) => {
                 setSaved(2);
                 // alert("Saved Successfully");
-                setTimeout(() => { setSaved(0) }, 3000)
+                setSuccess(1);
+                setTimeout(() => { setSuccess(0) }, 400);
+                setTimeout(() => { setSaved(0) }, 3000);
             })
             .catch((err) => console.log(err));
+    
+        }
     }
 
     return (
         <>
             <Score />
             <div className="save-score-page">
-                <input type="text" className="save-score-input" placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} />
+                <p>{success === 0 && success !== 1 ? "" : "Username cannot be empty." }</p>
+                <input type="text" className={`save-score-input ${success === 0 ? "" : success === 1 ? "save-score-input--success": 'save-score-input--failure' }`} placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} />
                 <SaveScore handleSave={handleSave} saved={saved} />
                 <Link to="/">
                     <GoBack />
